d43201fbe444a55f362a09c0304559ebbc87309b
ryguy806 <L1ghtSab3r11!!>
2020-07-23 09:57:25 -0700

    Added sounds to the keys.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     console.log(e.currentTarget.value);
+    const audio = new Audio(`sounds/piano_${e.currentTarget.value}.mp3`);
+    audio.play();
   }
 
   return (
